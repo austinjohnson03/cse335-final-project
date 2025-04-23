8bc393a0858b2b573c001a986ab857e163cc9671
austinjohnson03 <amj102998@gmail.com>
2025-04-23 14:06:05 -0400

    added several new tables; need to add line scores and officials games

--- schema.sql
+++ schema.sql
@@ -9,7 +9,6 @@ CREATE TABLE IF NOT EXISTS franchises (
 	is_active BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE INDEX idx_is_active ON franchises(is_active);
 
 CREATE TABLE IF NOT EXISTS teams (
 	id INT UNSIGNED PRIMARY KEY,
@@ -17,6 +16,22 @@ CREATE TABLE IF NOT EXISTS teams (
 	franchise_id INT UNSIGNED NOT NULL,
 	FOREIGN KEY (franchise_id) REFERENCES franchises(id)
 		ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS team_details (
+  id INT UNSIGNED PRIMARY KEY,
+  abbreviation VARCHAR(3) NOT NULL UNIQUE,
+  nickname VARCHAR(16) NOT NULL UNIQUE,
+  city VARCHAR(24) NOT NULL,
+  state CHAR(2) NOT NULL CHECK (state REGEXP '^[A-Z]{2}$'),
+  arena VARCHAR(32) NOT NULL CHECK,
+  arena_capacity INT UNSIGNED NOT NULL CHECK (arena_capacity > 0),
+  owner VARCHAR(64),
+  general_manager VARCHAR(64),
+  head_coach VARCHAR(64) NOT NULL,
+  d_league_affiliation VARCHAR(64) UNIQUE,
+  FOREIGN KEY (id) REFERENCES teams(id)
+    ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS seasons (
@@ -58,19 +73,80 @@ CREATE TABLE IF NOT EXISTS players (
 		ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS seasons (
-	id INT UNSIGNED PRIMARY KEY,
-	season_year INT UNSIGNED NOT NULL,
-	start_date DATE NOT NULL,
-	end_date DATE NOT NULL,
-	season_type ENUM(
-		'Preseason', 'Regular Season', 'Playoffs'
-		) NOT NULL
+CREATE TABLE IF NOT EXISTS draft_history (
+  player_id INT UNSIGNED,
+  season INT UNSIGNED NOT NULL,
+  round_number INT UNSIGNED,
+  round_pick INT UNSIGNED,
+  overall_pick INT UNSIGNED,
+  draft_type ENUM(
+    'Draft', 'Territorial'
+  ) NOT NULL,
+  team_id INT UNSIGNED NOT NULL,
+  organization VARCHAR(64),
+  organization_type ENUM(
+    'College/University', 'High School','Other Team/Club'
+  ),
+  PRIMARY KEY (player_id, season),
+  FOREIGN KEY (player_id) REFERENCES players(id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (team_id) REFERENCES teams(id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS games (
+  id INT UNSIGNED PRIMARY KEY,
+  season_id INT UNSIGNED NOT NULL,
+  game_date DATE NOT NULL,
+  attendance INT UNSIGNED NOT NULL,
+  game_time VARCHAR(5),
+  lead_changes INT UNSIGNED,
+  times_tied INT UNSIGNED,
+  FOREIGN KEY (season_id) REFERENCES seasons(id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS team_games (
+  game_id INT UNSIGNED NOT NULL,
+  team_id INT UNSIGNED NOT NULL,
+  fgm INT UNSIGNED,
+  fga INT UNSIGNED,
+  fg3m INT UNSIGNED,
+  fg3a INT UNSIGNED,
+  ftm INT UNSIGNED,
+  fta INT UNSIGNED,
+  oreb INT UNSIGNED,
+  dreb INT UNSIGNED,
+  reb INT UNSIGNED,
+  ast INT UNSIGNED,
+  stl INT UNSIGNED,
+  blk INT UNSIGNED,
+  tov INT UNSIGNED,
+  pf INT UNSIGNED,
+  pts INT UNSIGNED,
+  plus_minus INT,
+  pts_paint INT UNSIGNED,
+  pts_2nd_chance INT UNSIGNED,
+  pts_fb INT UNSIGNED,
+  largest_lead INT UNSIGNED,
+  team_turnovers INT UNSIGNED,
+  total_turnovers INT UNSIGNED,
+  team_rebounds INT UNSIGNED,
+  pts_off_to INT UNSIGNED,
+  is_home BOOLEAN,
+  PRIMARY KEY (game_id, team_id),
+  FOREIGN KEY (game_id) REFERENCES games(id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (team_id) REFERENCES teams(id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS officials (
+  id INT UNSIGNED PRIMARY KEY,
+  first_name VARCHAR(24) NOT NULL,
+  last_name VARCHAR(32) NOT NULL,
+  jersey_num INT UNSIGNED NOT NULL CHECK (jersey_num BETWEEN 0 AND 99)
 );
 
 
-CREATE TABLE IF NOT EXISTS games (
-	id INT UNSIGNED PRIMARY KEY,
-	season_id INT UNSIGNED NOT NULL,
-)
-
+CREATE INDEX idx_is_active ON franchises(is_active);
